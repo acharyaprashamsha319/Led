7ca0a52271ac9505cbbc894310c8e56b541ca527
SnakeGame <acharyaprashamsha26@gmail.com>
2021-12-08 10:27:15 +0545

    Initial commit

---
+++ src/Showpassword.java
@@ -0,0 +1,91 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Showpassword extends JFrame {
+    JLabel labelusername;
+    JLabel labelpassword;
+    JTextField user;
+    JPasswordField pass;
+    JButton Btn;
+    JCheckBox showpassword;
+
+
+    public Showpassword(){
+        setTitle("Login Page");
+        setSize(500,350);
+        setLocation(400,200);
+        setResizable(false);
+        setLayout(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        //for loginpage
+        labelusername = new JLabel("User Name");
+        labelusername.setForeground(Color.green);
+        labelusername.setFont(new Font("Arial", Font.BOLD,20));
+        labelusername.setBounds(50,50,200,20);
+        add(labelusername);
+
+        labelpassword = new JLabel("Password");
+        labelpassword.setForeground(Color.blue);
+        labelpassword.setFont(new Font("Arial", Font.BOLD,20));
+        labelpassword.setBounds(50,100,200,20);
+        add(labelpassword);
+
+        user = new JTextField();
+        user.setBounds(210,50,120,20);
+        add(user);
+        pass = new JPasswordField();
+        pass.setBounds(210,100,120,20);
+        add(pass);
+
+        showpassword =new JCheckBox("Show Password");
+        showpassword.setBounds(210,140,160,20);
+        showpassword.setForeground(Color.red);
+        showpassword.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if (showpassword.isSelected())
+                {
+                    pass.setEchoChar((char)0);
+                }else {
+                    pass.setEchoChar('*');
+                }
+            }
+        });
+        add(showpassword);
+
+        Btn = new JButton("LOGIN" );
+        Btn.setBounds(150,200,130,30);
+        Btn.setForeground(Color.magenta);
+        Btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource().equals(Btn))
+                {
+                    String username = user.getText();
+                    JOptionPane.showConfirmDialog(null,"Logged in as"+username);
+                    dispose();
+                    new HelloWorld().setVisible(true);
+
+
+                }
+
+            }
+        });
+        add(Btn);
+
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+        new Showpassword().setVisible(true);
+
+
+    }
+}
---
+++ src/Sign_Up.java
@@ -0,0 +1,87 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Sign_Up extends JFrame{
+    JLabel labelFirstName;
+    JLabel labelLastName;
+    JLabel labelpassword;
+    JLabel labelemail;
+    JLabel labelPhonenumber;
+    JButton Btn;
+    JTextField FirstName;
+    JTextField LastName;
+    JPasswordField password;
+    JTextField email;
+    JTextField number;
+    public Sign_Up(){
+        setTitle("Sign_Up Page");
+        setSize(500,350);
+        setLocation(100,100);
+        setResizable(false);
+        setLayout(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        //for loginpage
+        labelFirstName = new JLabel("First Name");
+        labelFirstName.setForeground(Color.black);
+        labelFirstName.setFont(new Font("Arial", Font.BOLD,20));
+        labelFirstName.setBounds(50,50,200,20);
+        add(labelFirstName);
+
+        labelLastName = new JLabel("Last Name");
+        labelLastName.setForeground(Color.black);
+        labelLastName.setFont(new Font("Arial", Font.BOLD,20));
+        labelLastName.setBounds(50,80,200,20);
+        add(labelLastName);
+
+        labelpassword = new JLabel("Password");
+        labelpassword.setForeground(Color.black);
+        labelpassword.setFont(new Font("Arial", Font.BOLD,20));
+        labelpassword.setBounds(50,110,200,20);
+        add(labelpassword);
+
+        labelemail = new JLabel("Phone Number");
+        labelemail.setBounds(40,150,280,20);
+        labelemail.setForeground(Color.black);
+        labelemail.setFont(new Font("Arial",Font.BOLD,20));
+        add(labelemail);
+
+
+        labelPhonenumber = new JLabel("Email address");
+        labelPhonenumber.setBounds(40,190,280,20);
+        labelPhonenumber.setForeground(Color.black);
+        labelPhonenumber.setFont(new Font("Arial",Font.BOLD,20));
+        add(labelPhonenumber);
+
+
+        FirstName = new JTextField();
+        FirstName.setBounds(180,50,120,20);
+        add(FirstName);
+        LastName = new JTextField();
+        LastName.setBounds(180,80,120,20);
+        add(LastName);
+        password = new JPasswordField();
+        password.setBounds(180,110,120,20);
+        add(password);
+        email= new JTextField();
+        email.setBounds(180,150,120,20);
+        add(email);
+        number= new JTextField();
+        number.setBounds(180,190,120,20);
+        add(number);
+
+
+        Btn = new JButton("SIGN UP" );
+        Btn.setBounds(160,240,150,30);
+        add(Btn);
+
+
+
+    }
+
+    public static void main(String[] args) {
+        new Sign_Up().setVisible(true);
+    }
+
+
+
+}
